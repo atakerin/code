b1dcfa7404d1c2c9b800bee771cee86229f4273a
Atakan Ebbhagen <atakerin@users.noreply.github.com>
2026-09-01 13:12:47 +0200

    den skriver ut mitt namn, ålder, längd och om jag gillar spel. coden visar också vilka typer det är

---
+++ variabler.py
@@ -0,0 +1,21 @@
+namn = "atakan"
+ålder = 18
+längd = 172.7
+gillar_att_spela = True
+
+print(namn)
+print(ålder)
+print(längd)
+print(gillar_att_spela)
+
+print(type(namn))
+print(type(ålder))
+print(type(längd))
+print(type(gillar_att_spela))
+
+poäng = 0
+print(poäng)
+poäng += 7
+print(poäng) 
+poäng += 3
+print(poäng)
