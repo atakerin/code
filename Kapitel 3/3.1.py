6c57c03ced0497557f6bbe15259ecdfc06b93e06
Atakan Ebbhagen <atakerin@users.noreply.github.com>
2026-09-18 10:54:37 +0200

    bytte namn

--- Kapitel 3/3.1.py
+++
@@ -1,8 +0,0 @@
-tid = int(input('Hur länge kommer du att ringa per månad? '))
-
-if tid <= 33:
-    print('Abonemangen "Kontant" är best för dig!')
-elif tid <= 66:
-     print('Abonemangen "normal" är best för dig!')
-else:
-     print('Abonemangen "plus" är best för dig!')
